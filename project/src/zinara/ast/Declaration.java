1b0d2687c5dc74f79b4b4f2f7d8cca473653f82c
Daniel Barreto <volrath.fp@gmail.com>
2010-02-25 17:10:42 -0430

    fixed declaration

--- project/src/zinara/ast/Declaration.java
+++ project/src/zinara/ast/Declaration.java
@@ -2,4 +2,17 @@ package zinara.ast;
 
 import zinara.ast.type.Type;
 
-public abstract class Declaration {}
+public class Declaration extends Declaration {
+    private Type type;
+    private String identifier;
+    private Object value;
+    private boolean variable;
+    
+    public Declaration(Type t, String id, Object v, boolean var) {
+	this.type = t;
+	this.identifier = id; this.value = v; this.variable = var;
+    }
+
+    public void setType(Type t) { this.type = t; }
+    public void setVariable(boolean var) { this.variable = var; }
+}
