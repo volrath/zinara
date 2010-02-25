38ee3bca4c1cc695a6be5b64c48a2394a214d82e
Daniel Barreto <volrath.fp@gmail.com>
2010-02-25 04:09:56 -0430

    unstable

--- project/src/zinara/ast/expression/CharExp.java
+++ project/src/zinara/ast/expression/CharExp.java
@@ -2,5 +2,5 @@ package zinara.ast.expression;
 
 public class CharExp extends Expression {
    public char value;
-   public IntegerExp ( char n ) { value=n; }
+   public CharExp ( char n ) { value=n; }
 }
