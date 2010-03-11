2356dbe5340f770e1ef9db57cd07a7a127557f5c
Daniel Barreto <volrath.fp@gmail.com>
2010-03-11 01:17:28 -0430

    Operators is working under the new Type convention

--- project/src/zinara/semantic/Operators.java
+++ project/src/zinara/semantic/Operators.java
@@ -1,9 +1,10 @@
 package zinara.semantic;
 
-import zinara.parser.sym;
-import zinara.exceptions.TypeClashException;
-
 import java.util.Hashtable;
+
+import zinara.ast.type.*;
+import zinara.exceptions.TypeClashException;
+import zinara.parser.sym;
 
 /*
   Does type cohersion and simple operator's type checking
@@ -11,9 +12,13 @@ import java.util.Hashtable;
 public class Operators {
     private class OP {
 	int operator; 
-	Integer rightType;
-	Integer leftType;
-	public OP(int o, Integer r, Integer l) { this.operator = o; this.rightType = r; this.leftType = l; }
+	Type rightType;
+	Type leftType;
+	public OP(int o, Type r, Type l) { this.operator = o; this.rightType = r; this.leftType = l; }
+
+	public boolean equals(OP other) {
+	    return (this.leftType == other.leftType) && (this.rightType == other.rightType);
+	}
     }
 
     public Hashtable table;
@@ -27,42 +32,42 @@ public class Operators {
 	  Binary Operators
 	 */
 	// Arithmetic
-	this.table.put(this.new OP(this.arithmetic, basicTypes.Int, basicTypes.Int), basicTypes.Int);
-	this.table.put(new OP(this.arithmetic, basicTypes.Float, basicTypes.Float), basicTypes.Float);
-	this.table.put(new OP(this.arithmetic, basicTypes.Int, basicTypes.Float), basicTypes.Float);
-	this.table.put(new OP(this.arithmetic, basicTypes.Float, basicTypes.Int), basicTypes.Float);
-	//this.table.put(new OP(sym.PLUS, basicTypes.Char, basicTypes.Char), STRING);
+	this.table.put(this.new OP(this.arithmetic, new IntType(), new IntType()), new IntType()); // new IntType() -> new IntType()
+	this.table.put(new OP(this.arithmetic, new FloatType(), new FloatType()), new FloatType());
+	this.table.put(new OP(this.arithmetic, new IntType(), new FloatType()), new FloatType());
+	this.table.put(new OP(this.arithmetic, new FloatType(), new IntType()), new FloatType());
+	//this.table.put(new OP(sym.PLUS, new CharType(), new CharType()), STRING);
 
 	// Relational
-	this.table.put(new OP(this.relational, basicTypes.Int, basicTypes.Int), basicTypes.Bool);
-	this.table.put(new OP(this.relational, basicTypes.Float, basicTypes.Float), basicTypes.Bool);
-	this.table.put(new OP(this.relational, basicTypes.Char, basicTypes.Char), basicTypes.Bool);
-	this.table.put(new OP(this.relational, basicTypes.Bool, basicTypes.Bool), basicTypes.Bool);
-	this.table.put(new OP(this.relational, basicTypes.Int, basicTypes.Float), basicTypes.Bool);
-	this.table.put(new OP(this.relational, basicTypes.Float, basicTypes.Int), basicTypes.Bool);
+	this.table.put(new OP(this.relational, new IntType(), new IntType()), new BoolType());
+	this.table.put(new OP(this.relational, new FloatType(), new FloatType()), new BoolType());
+	this.table.put(new OP(this.relational, new CharType(), new CharType()), new BoolType());
+	this.table.put(new OP(this.relational, new BoolType(), new BoolType()), new BoolType());
+	this.table.put(new OP(this.relational, new IntType(), new FloatType()), new BoolType());
+	this.table.put(new OP(this.relational, new FloatType(), new IntType()), new BoolType());
 
 	// Logical
-	this.table.put(new OP(this.logical, basicTypes.Int, basicTypes.Int), basicTypes.Bool);
-	this.table.put(new OP(this.logical, basicTypes.Float, basicTypes.Float), basicTypes.Bool);
-	this.table.put(new OP(this.logical, basicTypes.Bool, basicTypes.Bool), basicTypes.Bool);
-	this.table.put(new OP(this.logical, basicTypes.Int, basicTypes.Float), basicTypes.Bool);
-	this.table.put(new OP(this.logical, basicTypes.Float, basicTypes.Int), basicTypes.Bool);
+	this.table.put(new OP(this.logical, new IntType(), new IntType()), new BoolType());
+	this.table.put(new OP(this.logical, new FloatType(), new FloatType()), new BoolType());
+	this.table.put(new OP(this.logical, new BoolType(), new BoolType()), new BoolType());
+	this.table.put(new OP(this.logical, new IntType(), new FloatType()), new BoolType());
+	this.table.put(new OP(this.logical, new FloatType(), new IntType()), new BoolType());
 
 	/*
 	  Unary Operators
 	 */
-	this.table.put(new OP(sym.NOEQ, basicTypes.Int, null), basicTypes.Bool);
-	this.table.put(new OP(sym.NOEQ, basicTypes.Float, null), basicTypes.Bool);
-	this.table.put(new OP(sym.NOEQ, basicTypes.Bool, null), basicTypes.Bool);
+	this.table.put(new OP(sym.NOEQ, new IntType(), null), new BoolType());
+	this.table.put(new OP(sym.NOEQ, new FloatType(), null), new BoolType());
+	this.table.put(new OP(sym.NOEQ, new BoolType(), null), new BoolType());
 
-	this.table.put(new OP(sym.UMINUS, basicTypes.Int, null), basicTypes.Int);
-	this.table.put(new OP(sym.UMINUS, basicTypes.Float, null), basicTypes.Float);
+	this.table.put(new OP(sym.UMINUS, new IntType(), null), new IntType());
+	this.table.put(new OP(sym.UMINUS, new FloatType(), null), new FloatType());
     }
 
     // also returns the type cohersion if needed
-    public Integer check(int o, Integer r, Integer l) throws TypeClashException {
+    public Type check(int o, Type r, Type l) throws TypeClashException {
 	try {
-	    return (Integer)this.table.get(new OP(o,r,l));
+	    return (Type)this.table.get(new OP(o,r,l));
 	} catch (NullPointerException e) {
 	    throw new TypeClashException("Error de tipos con el operador " + o + " en la linea tal..."); // mejorar
 	}
