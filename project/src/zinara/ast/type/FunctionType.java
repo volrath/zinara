53f214d72d9ac1ae713bba8d1f31b701db50d550
Daniel Barreto <volrath.fp@gmail.com>
2010-03-11 01:16:44 -0430

    another iteration over FunctionType, it includes now the CodeBlock of the function

--- project/src/zinara/ast/type/FunctionType.java
+++ project/src/zinara/ast/type/FunctionType.java
@@ -2,9 +2,16 @@ package zinara.ast.type;
 
 import java.util.ArrayList;
 
+import zinara.ast.instructions.CodeBlock;
+
 public class FunctionType extends Type {
     public ArrayList argsTypes; // arraylist of Type
     public Type returnType;
+    public CodeBlock codeBlock;
 
-    public FunctionType(ArrayList al, Type rt) { this.argsTypes = al; this.returnType = rt; }
+    public FunctionType(ArrayList al, Type rt, CodeBlock cb) { 
+	this.argsTypes = al;
+	this.returnType = rt;
+	this.codeBlock = cb;
+    }
 }
