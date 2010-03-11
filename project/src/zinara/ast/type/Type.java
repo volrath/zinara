cfa6a8925219a2efc5d6d6a3452d02521c95ad11
Daniel Barreto <volrath.fp@gmail.com>
2010-03-11 01:16:02 -0430

    now the type class has override the equal method, it will check only if the two instances are instances of the same Type-subclass

--- project/src/zinara/ast/type/Type.java
+++ project/src/zinara/ast/type/Type.java
@@ -2,6 +2,20 @@ package zinara.ast.type;
 
 public abstract class Type {
     public boolean equals(Type other) {
-	return true;
+	/*
+	  Basic Types
+	*/
+	if ((this instanceof IntType && other instanceof IntType) || 
+	    (this instanceof FloatType && other instanceof FloatType) ||
+	    (this instanceof CharType && other instanceof CharType) ||
+	    (this instanceof BoolType && other instanceof BoolType))
+	    return true;
+	else {
+	    /*
+	      Composed Types
+	     */
+	    //if (this instanceof ListType)
+	    return false;
+	}
     }
 }
