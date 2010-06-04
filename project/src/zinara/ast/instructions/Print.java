8e707d8344ba1788784b6861ae8a63c1ed7b4603
German <plaga701@gmail.com>
2010-06-04 01:00:16 -0430

    - Print.java:   . Introducido de nuevo el viejo print para hacer ciertas pruebas

--- project/src/zinara/ast/instructions/Print.java
+++ project/src/zinara/ast/instructions/Print.java
@@ -26,37 +26,60 @@ public class Print extends Instruction{
     }
 
     public void tox86(Genx86 generate) throws IOException,InvalidCodeException{
-	//Por ahora solo sirve con numeros, bools, chars y flotantes
-	expr.register = register;
-	String expReg = generate.addrRegName(expr.register);
+	// //Por ahora solo sirve con numeros, bools, chars y flotantes
+	// expr.register = register;
+	// String expReg = generate.addrRegName(expr.register);
 
-	expr.tox86(generate);
+	// expr.tox86(generate);
 
-	generate.write(generate.push("rdi"));
+	// generate.write(generate.push("rdi"));
 
-	if (expr.type.getType() instanceof IntType){
-	    generate.write(generate.movInt("rdi",expReg));
-	    generate.write("call print_int\n");
-	}
-	else if (expr.type.getType() instanceof BoolType){
-	    generate.write(generate.movBool("rdi",expReg));
-	    generate.write("call print_int\n");
-	}
-	else if (expr.type.getType() instanceof FloatType){
-	    generate.write(generate.movReal("rdi",expReg));
-	    generate.write("call print_float\n");
-	}
-	else if (expr.type.getType() instanceof CharType){
-	    generate.write(generate.movChar("rdi",expReg));
-	    generate.write("call print_char\n");
-	}
-	else{
-	    generate.write("print de "+expr.type.getType()+" no implementado");
-	    System.out.println("====================");
-	    System.out.println("print de "+expr.type.getType()+" no implementado");
-	    System.out.println("====================");
-	}
+	// if (expr.type.getType() instanceof IntType){
+	//     generate.write(generate.movInt("rdi",expReg));
+	//     generate.write("call print_int\n");
+	// }
+	// else if (expr.type.getType() instanceof BoolType){
+	//     generate.write(generate.movBool("rdi",expReg));
+	//     generate.write("call print_int\n");
+	// }
+	// else if (expr.type.getType() instanceof FloatType){
+	//     generate.write(generate.movReal("rdi",expReg));
+	//     generate.write("call print_float\n");
+	// }
+	// else if (expr.type.getType() instanceof CharType){
+	//     generate.write(generate.movChar("rdi",expReg));
+	//     generate.write("call print_char\n");
+	// }
+	// else{
+	//     generate.write("print de "+expr.type.getType()+" no implementado");
+	//     System.out.println("====================");
+	//     System.out.println("print de "+expr.type.getType()+" no implementado");
+	//     System.out.println("====================");
+	// }
 	
-	generate.write(generate.pop("rdi"));
+	// generate.write(generate.pop("rdi"));
+
+       // Por ahora se asume que todas las expresiones son numeros enteros
+        //de un solo digito.
+        String code = "";
+        String expReg = generate.regName(register,expr.type);
+
+        expr.tox86(generate);
+
+        //"Transformo" a ASCII
+        code += generate.add(expReg,"48");
+
+        code += generate.save_print_regs();
+
+        //Necesita ser generico, esperando que asm_io funcione
+        code += generate.movInt("["+generate.stack_pointer()+"]",expReg);
+        // Pongo el valor de la expresion en la pila, ya que la llamada
+        //sys_write necesita que el String este en memoria.
+        
+        code += generate.setup_print(generate.stack_pointer(),"1","4");
+        code += generate.syscall();
+        code += generate.restore_print_regs();
+                    
+        generate.write(code);
     }
 }
