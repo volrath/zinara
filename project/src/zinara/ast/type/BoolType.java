3f85a6e73e33f74626a98e1a10d287dd82adce4f
Daniel Barreto <volrath.fp@gmail.com>
2010-07-01 23:19:14 -0430

    newtyping and aliasing done... is not tested, but it has all my believes and hopes =), so that should be enough

--- project/src/zinara/ast/type/BoolType.java
+++ project/src/zinara/ast/type/BoolType.java
@@ -4,8 +4,16 @@ public class BoolType extends Type {
     public int size = 1;
 
     public BoolType() {}
-    public String toString() { return "<BOOL>"; }
-    public boolean equals(Type other) { return (other.getType() instanceof BoolType) && other.getName().equals(name); }
+    public String toString() { return (name.equals("") ? "<BOOL>" : "<" + name + ">"); }
+    public boolean equals(Type other) {
+	if (other != null && (other.getType() instanceof BoolType))
+	    if (!other.getName().equals("") && !name.equals(""))
+		return  other.getName().equals(name);
+	    else
+		return true;
+	else
+	    return false;
+    }
     public Type getType() { return this; }
     public int size() { return this.size; }
     public void setName(String n) { name = n; }
--- project/src/zinara/ast/type/TupleType.java
+++ project/src/zinara/ast/type/TupleType.java
@@ -20,6 +20,7 @@ public class TupleType extends Type {
     public Type get(int i) { return (Type)types.get(i); }
 
     public String toString() {
+	if (!name.equals("")) return "<" + name + ">";
 	String ret = "<(";
 	for (int i = 0; i < types.size(); i++)
 	    ret += (Type)types.get(i) + ", ";
@@ -29,9 +30,10 @@ public class TupleType extends Type {
     public Type getType() { return this; }
 
     public boolean equals(Type other) {
-	if (!(other instanceof TupleType)) return false;
+	if (other != null && !(other instanceof TupleType)) return false;
 	TupleType otherTuple = (TupleType)other;
-	if (otherTuple.len() == 0) return true;
+	//if (otherTuple.len() == 0) return true;
+	if (!otherTuple.getName().equals("") && !name.equals("")) return  otherTuple.getName().equals(name);
 	if (len() != otherTuple.len()) return false;
 	for (int i = 0; i < len(); i++)
 	    if (!get(i).equals(otherTuple.get(i))) return false;
